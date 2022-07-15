d81d0a7c3f0ad8a2a8c9f1ebdd438afa253a1c34
Ahammar Yassine <yassine@tousfacteurs.com>
2022-07-15 14:20:31 +0200

    Add logout method

--- app/src/Controller/LogoutController.php
+++ app/src/Controller/LogoutController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 class LogoutController extends BaseController
 {
 
-    public function index()
+    public function index(ServerRequestInterface $request): ResponseInterface
     {
         session_unset();
         session_destroy();
         $_SESSION = array();
 
-        header("Location: /");
+        return new Response\RedirectResponse('/');
     }
 }
